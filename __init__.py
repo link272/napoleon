c1d78bc18103cea5f5518422776b0648d643767b
Ben <link272@users.noreply.github.com>
2021-06-23 19:50:52 +0200

    remove unused code

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 from napoleon.core.vault.fernet import FernetVault
 from napoleon.core.utils.machine import Platform
-from napoleon.core.trace.trace import Tracer
+from napoleon.core.log.trace import Tracer
 from napoleon.core.application import Application
