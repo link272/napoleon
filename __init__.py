c2cc09089a1d7ed0396d3868aa4272696eb45c4c
Ben <link272@users.noreply.github.com>
2021-06-24 07:14:27 +0200

    root import

--- __init__.py
+++ __init__.py
@@ -2,3 +2,8 @@ from napoleon.core.vault.fernet import FernetVault
 from napoleon.core.utils.machine import Platform
 from napoleon.core.log.trace import Tracer
 from napoleon.core.application import Application
+from napoleon.core.special.path import FilePath, Path
+from napoleon.properties import AbstractObject,  Float, Integer, String, Boolean, Bytes, JSON, Symbol, UUID, DateTime,\
+    PlaceHolder, List, Map, Set, Instance
+from napoleon.tools.singleton import Nothing, exist, Undefined, is_define
+from napoleon.core.special.alias import Alias, MapAlias
